b221d21191f80333cdeda8632451f916f9dafce3
Yujev <ustatar@ya.ru>
2022-09-22 21:00:36 +0300

    Add Zadacha_3

---
+++ Zadacha_3/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Координаты для Х1");
+int x1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Координаты для Y1");
+int y1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Координаты для Х2");
+int x2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Координаты для Y2");
+int y2 = int.Parse(Console.ReadLine());
+
+double A = Math.Sqrt(Math.Pow()x1-x2),)
