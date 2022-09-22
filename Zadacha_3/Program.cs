25b254fc9ae622296d1769ff724e2119c1e83d3b
Yujev <ustatar@ya.ru>
2022-09-23 00:15:26 +0300

    Zadacha_3

--- Zadacha_3/Program.cs
+++ Zadacha_3/Program.cs
@@ -1,9 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Напишите программу, которая на вход принимает координаты двух точек и находит расстояние между ними в 2Д пространстве
+
 Console.WriteLine("Координаты для Х1");
 int x1 = int.Parse(Console.ReadLine());
+//int y = int.Parse(Console.ReadLine());
 
 Console.WriteLine("Координаты для Y1");
 int y1 = int.Parse(Console.ReadLine());
+//int y = int.Parse(Console.ReadLine());
 
 Console.WriteLine("Координаты для Х2");
 int x2 = int.Parse(Console.ReadLine());
@@ -11,4 +14,9 @@ int x2 = int.Parse(Console.ReadLine());
 Console.WriteLine("Координаты для Y2");
 int y2 = int.Parse(Console.ReadLine());
 
-double A = Math.Sqrt(Math.Pow()x1-x2),)
+double A = Math.Sqrt(Math.Pow((x1-x2),2) +Math.Pow((y1-y2),2));
+Console.WriteLine($"Расстояние между точками: {A:f2}");
+
+/*Второе решение
+double result = Math.Sqrt(Math.Pow(x2 - x, 2) + Math.Pow(y2 - y, 2));
+Console.WriteLine($"Расстояние между точками: -> {result:f2}");*/
