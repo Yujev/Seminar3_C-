0b0065ec8c270fd77b281c3a3c23e36e5994470c
Yujev <112649496+Yujev@users.noreply.github.com>
2022-09-23 16:37:42 +0300

    Update Program.cs

--- Zadacha_1/Program.cs
+++ Zadacha_1/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+/* Напиши программу, которая по заданному номеру четверти, показывает диапазон
+возможных координат этой четверти (х и у).
+*/
+
+Console.WriteLine("Первая четверть: ");
+int number = int.Parse(Console.ReadLine());
+switch (number)
+{
+    case 1:
+        {
+            Console.WriteLine("Диапазон возможных значений 1 четверти x > 0, y > 0");
+            break
+        }
+
+    case 2:
+        {
+            Console.WriteLine("Диапазон возможных значений 2 четверти x < 0, y > 0");
+            break
+        }
+    case 3:
+        {
+            Console.WriteLine("Диапазон возможных значений 3 четверти x < 0, y < 0");
+            break
+        }
+
+    case 4:
+        {
+            Console.WriteLine("Диапазон возможных значений 4 четверти x > 0, y < 0");
+            break
+        }
+    default:
+        {
+            Console.WriteLine("Такой четверти нет. Будь вниметельней! ");
+            break;
+        }
+}
