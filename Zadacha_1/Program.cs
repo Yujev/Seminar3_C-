1a709065f6f6d5bb672ac8bb89fde36928ef2b9d
Yujev <112649496+Yujev@users.noreply.github.com>
2022-09-23 16:52:21 +0300

    Update Program.cs

--- Zadacha_1/Program.cs
+++ Zadacha_1/Program.cs
@@ -2,31 +2,31 @@
 возможных координат этой четверти (х и у).
 */
 
-Console.WriteLine("Первая четверть: ");
+Console.WriteLine("Введи четверть: ");
 int number = int.Parse(Console.ReadLine());
 switch (number)
 {
     case 1:
         {
             Console.WriteLine("Диапазон возможных значений 1 четверти x > 0, y > 0");
-            break
+            break;
         }
 
     case 2:
         {
             Console.WriteLine("Диапазон возможных значений 2 четверти x < 0, y > 0");
-            break
+            break;
         }
     case 3:
         {
             Console.WriteLine("Диапазон возможных значений 3 четверти x < 0, y < 0");
-            break
+            break;
         }
 
     case 4:
         {
             Console.WriteLine("Диапазон возможных значений 4 четверти x > 0, y < 0");
-            break
+            break;
         }
     default:
         {
